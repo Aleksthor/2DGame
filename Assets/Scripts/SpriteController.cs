c3f6f02d8d9bb4f3372ffbb15b251a685300f1c3
Aleksthor <athorset98@gmail.com>
2022-08-31 00:42:04 +0200

    Revert "Error Fix"
    This reverts commit 9ed0b77b3fa2f4e0767e7bf695fd7f988909a2f2.

--- Assets/Scripts/SpriteController.cs
+++ Assets/Scripts/SpriteController.cs
@@ -2,65 +2,138 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class SpriteController : MonoBehaviour
 {
     Rigidbody2D Rigidbody;
     Animator animator;
+    public SpriteRenderer BodySprite;
+    public SpriteRenderer HeadSprite;
+    public SpriteRenderer HandSprite;
+    public SpriteRenderer HatSprite;
+    public SpriteRenderer WeaponSprite;
+    public SpriteRenderer ShieldSprite;
+    public SpriteRenderer EffectsSprite;
+
+    public Transform Hand;
+    public Transform Shield;
+    public Transform Effects;
+    public Transform Weapon;
 
     public Vector2 MovementVector;
-    public float MovementSpeed = 2f;
-
-    bool CanMove = true;
- 
-
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-        Rigidbody = GetComponent<Rigidbody2D>();
-    }
-    // Start is called before the first frame update
+  
+    bool FlipLastInput = false;
+    bool CanTurn = true;
     void Update()
     {
         MovementVector.x = Input.GetAxis("Horizontal");
         MovementVector.y = Input.GetAxis("Vertical");
-
-
-
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+
+    void LateUpdate()
     {
-        if (MovementVector.x != 0 || MovementVector.y != 0)
+        if (CanTurn)
         {
-            animator.SetBool("Moving", true);
+            if (MovementVector.x > 0 || FlipLastInput)
+            {
+
+                BodySprite.flipX = true;
+                HeadSprite.flipX = true;
+                HandSprite.flipX = true;
+                ShieldSprite.flipX = true;
+                HatSprite.flipX = true;
+                WeaponSprite.flipX = true;
+                EffectsSprite.flipX = true;
+
+
+                Hand.transform.localPosition = new Vector3(Hand.transform.localPosition.x * -1f, Hand.transform.localPosition.y, Hand.transform.localPosition.z);
+                Shield.transform.localPosition = new Vector3(Shield.transform.localPosition.x * -1f, Shield.transform.localPosition.y, Shield.transform.localPosition.z);
+                Effects.transform.localPosition = new Vector3(Effects.transform.localPosition.x * -1f, Effects.transform.localPosition.y, Effects.transform.localPosition.z);
+                Weapon.transform.eulerAngles = new Vector3(Weapon.transform.eulerAngles.x, Weapon.transform.eulerAngles.y, Weapon.transform.eulerAngles.z * -1f);
+
+
+                FlipLastInput = true;
+            }
+            if (MovementVector.x < 0 || !FlipLastInput)
+            {
+                BodySprite.flipX = false;
+                HeadSprite.flipX = false;
+                HandSprite.flipX = false;
+                ShieldSprite.flipX = false;
+                HatSprite.flipX = false;
+                WeaponSprite.flipX = false;
+                EffectsSprite.flipX = false;
+
+                Hand.transform.localPosition = new Vector3(Hand.transform.localPosition.x, Hand.transform.localPosition.y, Hand.transform.localPosition.z);
+                Shield.transform.localPosition = new Vector3(Shield.transform.localPosition.x, Shield.transform.localPosition.y, Shield.transform.localPosition.z);
+                Effects.transform.localPosition = new Vector3(Effects.transform.localPosition.x, Effects.transform.localPosition.y, Effects.transform.localPosition.z);
+                Weapon.transform.eulerAngles = new Vector3(Weapon.transform.eulerAngles.x, Weapon.transform.eulerAngles.y, Weapon.transform.eulerAngles.z);
+
+                FlipLastInput = false;
+
+            }
         }
         else
         {
-            animator.SetBool("Moving", false);
+            if (FlipLastInput)
+            {
+                BodySprite.flipX = true;
+                HeadSprite.flipX = true;
+                HandSprite.flipX = true;
+                ShieldSprite.flipX = true;
+                HatSprite.flipX = true;
+                WeaponSprite.flipX = true;
+                EffectsSprite.flipX = true;
+
+                Hand.transform.localPosition = new Vector3(Hand.transform.localPosition.x * -1f, Hand.transform.localPosition.y, Hand.transform.localPosition.z);
+                Shield.transform.localPosition = new Vector3(Shield.transform.localPosition.x * -1f, Shield.transform.localPosition.y, Shield.transform.localPosition.z);
+                Effects.transform.localPosition = new Vector3(Effects.transform.localPosition.x * -1f, Effects.transform.localPosition.y, Effects.transform.localPosition.z);
+
+
+
+                FlipLastInput = true;
+
+
+            }
+            if (!FlipLastInput)
+            {
+                BodySprite.flipX = false;
+                HeadSprite.flipX = false;
+                HandSprite.flipX = false;
+                ShieldSprite.flipX = false;
+                HatSprite.flipX = false;
+                WeaponSprite.flipX = false;
+                EffectsSprite.flipX = false;
+
+                Hand.transform.localPosition = new Vector3(Hand.transform.localPosition.x, Hand.transform.localPosition.y, Hand.transform.localPosition.z);
+                Shield.transform.localPosition = new Vector3(Shield.transform.localPosition.x, Shield.transform.localPosition.y, Shield.transform.localPosition.z);
+                Effects.transform.localPosition = new Vector3(Effects.transform.localPosition.x, Effects.transform.localPosition.y, Effects.transform.localPosition.z);
+
+
+                FlipLastInput = false;
+
+            }
+
+
+
+
         }
-        if (CanMove)
-        {
-            Rigidbody.MovePosition(Rigidbody.position + (MovementVector * MovementSpeed * Time.fixedDeltaTime));
-        }
-
-       
-
-
-
-        
     }
+
+
 
 
     public void StartAttack()
     {
-        CanMove = false;
+        
+        CanTurn = false;
     }
 
 
     public void StopAttack()
     {
-        CanMove = true;
+       
+        CanTurn = true;
     }
 
 }
